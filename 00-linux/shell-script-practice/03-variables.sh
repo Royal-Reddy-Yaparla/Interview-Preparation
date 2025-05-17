798e8886fab509e01febe7a458cc811c69d865ee
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-17 22:34:42 +0530

    conversion

---
+++ 00-linux/shell-script-practice/03-variables.sh
@@ -0,0 +1,12 @@
+
+PLAYER=Kohil
+BOARD=BCCI
+
+echo "$PLAYER: Hello! Board"
+echo "$BOARD: Hi! nice to meet $PLAYER"
+echo "$BOARD: how is going IPL, I think your fitness is good"
+echo "$PLAYER: Yeah , I want to say , going to retirement from test format" 
+echo "$BOARD: No $PLAYER , we need you , Please stay with us"
+echo "$PLAYER: No $BOARD , I want to focus on only ODIs so took this decision."
+echo "$BOARD: We respect your decision $PLAYER , we are thankful to your contribution"
+echo "$PLAYER: Thank you!"
