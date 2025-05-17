3ce757414aa8842083f14b748d1890235fce0757
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-17 23:08:08 +0530

    shell-practice

---
+++ 00-linux/shell-script-practice/07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DEVOPS_STACK=(Linux AWS GIT Ansible Terraform Jenkins Docker Kubernetes Promethues Grafana ELK)
+
+echo "DevOps Stack: ${DEVOPS_STACK[@]}"
+
+echo "Operating System: $DEVOPS_STACK[0]"
+
+echo "Cloud: $DEVOPS_STACK[1]"
+
+echo "Configuration Management: $DEVOPS_STACK[2]"
