67e5c8fcd7e304ad486569fe1ec6135ce598b9d6
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-17 22:32:03 +0530

    conversion

---
+++ 00-linux/shell-script-practice/02-conversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Kohli: Hello! Board"
+echo "BCCI: Hi! nice to meet Kohli"
+echo "BCCI: how is going IPL, I think your fitness is good"
+echo "Kohli: Yeah , I want to say , going to retirement from test format" 
+echo "BCCI: No Kohli , we need you , Please stay with us"
+echo "Kohli: No BCCI , I want to focus on only ODIs so took this decision."
+echo "BCCI: We respect your decision Kohli , we are thankful to your contribution"
+echo "Kohli: Thank you!"
