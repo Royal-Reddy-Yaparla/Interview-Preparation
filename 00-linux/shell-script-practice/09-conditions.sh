40fac3e7b14c4422ad9992d4c9c041016b1530cc
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-17 23:36:43 +0530

    shell-practice

--- 00-linux/shell-script-practice/09-conditions.sh
+++ 00-linux/shell-script-practice/09-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Welcome to the game , enter the a numeric number to find to is positive or negative"
-INPUT=$1
+read INPUT
 
 if [ $INPUT -gt 0 ]; then 
     echo "It is postive"
