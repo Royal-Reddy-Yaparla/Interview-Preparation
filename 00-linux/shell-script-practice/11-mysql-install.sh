a6ccc431a2f816d450a8b1a7e88972af40830e3f
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-18 20:12:17 +0530

    shell-practice

--- 00-linux/shell-script-practice/11-mysql-install.sh
+++ 00-linux/shell-script-practice/11-mysql-install.sh
@@ -3,16 +3,6 @@
 USER_ID=$(id -u)
 
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]; then 
-#         echo "$2 process is failed"
-#         exit 1
-#     else
-#         echo "$2 process is success"
-#     fi
-# }
-
-
 if [ $USER_ID -ne 0 ]
 then
     echo "ERROR: need to provide sudo user access"
@@ -23,14 +13,14 @@ dnf list installed mysql
 
 if [ $? -eq 0 ]
 then 
-    echo "mysql is already installed"
+    echo "INFO: mysql is already installed"
     exit 1
 else
-    echo "mysql is not installed , going to install"
+    echo "INFO: mysql is not installed , going to install"
     dnf install mysql-server -y
     if [ $? -eq 0 ]
     then
-        echo "MySql is installed Successfully"
+        echo "INFO: MySql is installed Successfully"
     else
         echo "ERROR: MySql installation is Failed"
         exit 1
