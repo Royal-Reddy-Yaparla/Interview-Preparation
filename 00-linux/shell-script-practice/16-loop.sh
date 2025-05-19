157d7388816d66a5113e8d9da3be5af40fe66a1b
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-19 18:59:53 +0530

    shell-practice

---
+++ 00-linux/shell-script-practice/16-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for num in {1..20}
+do 
+    echo "count: $num"
+done
+
+
+for number in 1 2 3 4 5
+do 
+    echo "count: $number"
+done
+
+
+for package in mysql python3 nginx
+do 
+    echo "count: $package"
+done
