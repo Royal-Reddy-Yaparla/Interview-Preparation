2ea29d4a5da1eb605aad567aa0cd69469280a935
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-19 16:29:27 +0530

    shell-practice

--- 00-linux/shell-script-practice/12-color.sh
+++ 00-linux/shell-script-practice/12-color.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo -e "\e[31m I am RED Color"
+
+echo -e "\e[32m I am GREEN Color"
+
+echo -e "\e[33m I am YELLOW Color"
+
+echo -e "\e[34m I am BLUE Color"
+
+echo -e "\e[35m I am MAGNETA Color"
+
+echo -e "\e[36m I am CYAN Color"
+
+echo -e "\e[37m I am WHITE Color"
+
+echo -e "\e[0m I am NO Color"
+
