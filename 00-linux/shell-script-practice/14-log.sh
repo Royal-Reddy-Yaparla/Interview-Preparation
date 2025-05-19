5f138113ed89e3f223a90667eef545db66dd3d92
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-19 17:47:24 +0530

    shell-practice

--- 00-linux/shell-script-practice/14-log.sh
+++ 00-linux/shell-script-practice/14-log.sh
@@ -10,8 +10,9 @@ N="\e[0m"
 USER_ID=$(id -u)
 LOG_REPO="/var/log/package-install-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$SCRIPT_NAME.log"
 
-echo "$SCRIPT_NAME"
+echo "$LOG_FILE"
 
 # if [ $USER_ID -ne 0 ]
 # then
