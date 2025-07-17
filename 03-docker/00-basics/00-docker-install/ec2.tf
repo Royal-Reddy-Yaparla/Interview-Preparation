a429eca7248016a118f7904fe7c1d1f1a92c2fac
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-07-17 06:50:00 +0530

    docker-practice

--- 03-docker/00-basics/00-docker-install/ec2.tf
+++ 03-docker/00-basics/00-docker-install/ec2.tf
@@ -2,21 +2,23 @@ resource "aws_instance" "my_instance" {
   ami                    = data.aws_ami.custom_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_all.id]
-  # provisioner "remote-exec" {
-  #   connection {
-  #     type     = "ssh"
-  #     user     = "ec2-user"
-  #     password = "DevOps321"
-  #     host     = self.public_ip
-  #   }
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
 
-  #   inline = [
-  #     "sudo dnf -y install dnf-plugins-core",
-  #     "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
-  #     "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-  #     "sudo usermod -aG docker ec2-user",
-  #   ]
-  # }
+    inline = [
+      "sudo dnf -y install dnf-plugins-core",
+      "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
+      "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo usermod -aG docker ec2-user"
+    ]
+  }
 
   tags = merge( # to merge maps
     var.common_tags,
@@ -28,30 +30,27 @@ resource "aws_instance" "my_instance" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = var.sg_name
-  description = var.sg_description
+  name        = "allow_all"
+  description = "allow ing all inbound rules"
   # vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "allow_tls"
   }
 
-  dynamic "ingress" {
-    for_each = var.ingress_ports
-    content {
-      from_port   = ingress.value["from_port"]
-      to_port     = ingress.value["to_port"]
-      protocol    = ingress.value["ingress_protocol"]
-      cidr_blocks = var.ingress_cidr_blocks
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     # ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port   = var.egress_from_port
-    to_port     = var.egress_to_port
-    protocol    = var.egress_from_port
-    cidr_blocks = var.egress_cidr_blocks
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     # ipv6_cidr_blocks = ["::/0"]
   }
 }
